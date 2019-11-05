b1a96cd760bf2e92592bed34512b1b96eba5e963
UVR_JJW_PC\UVR-KAIST <zkrkwlek@gmail.com>
2019-11-05 13:51:19 +0900

    #outdoor color 추가

--- include/SegmentationData.h
+++ include/SegmentationData.h
@@ -16,14 +16,23 @@ namespace UVR_SLAM {
 	const cv::Vec3b COLOR_FLOOR = cv::Vec3b(0, 0, 128);
 	const cv::Vec3b COLOR_CEILING = cv::Vec3b(0, 128, 128);
 
+	const cv::Vec3b COLOR_OUTDOOR_NONE = cv::Vec3b(255, 255, 255);
+	const cv::Vec3b COLOR_OUTDOOR_ROAD = cv::Vec3b(0, 0, 0);
+	const cv::Vec3b COLOR_OUTDOOR_BUILDING = cv::Vec3b(0, 128, 0);
+	const cv::Vec3b COLOR_OUTDOOR_SKY = cv::Vec3b(64, 128, 0);
+	
 	class ObjectColors {
 	public:
 		static std::vector<cv::Vec3b> mvObjectLabelColors;
 		static void Init() {
-			mvObjectLabelColors.push_back(COLOR_FLOOR);
+			/*mvObjectLabelColors.push_back(COLOR_FLOOR);
 			mvObjectLabelColors.push_back(COLOR_WALL);
 			mvObjectLabelColors.push_back(COLOR_CEILING);
-			mvObjectLabelColors.push_back(COLOR_NONE);
+			mvObjectLabelColors.push_back(COLOR_NONE);*/
+			mvObjectLabelColors.push_back(COLOR_OUTDOOR_ROAD);
+			mvObjectLabelColors.push_back(COLOR_OUTDOOR_BUILDING);
+			mvObjectLabelColors.push_back(COLOR_OUTDOOR_SKY);
+			mvObjectLabelColors.push_back(COLOR_OUTDOOR_NONE);
 		}
 	};
 
