4861bce4ee2de0119aa2d5e31556dcb10dc841e8
UVR_JJW_PC\UVR-KAIST <zkrkwlek@gmail.com>
2020-10-20 23:59:20 +0900

    #201020 KF-KF 매칭 기본 버전

--- src/Tracker.cpp
+++ src/Tracker.cpp
@@ -137,8 +137,6 @@ void UVR_SLAM::Tracker::Tracking(Frame* pPrev, Frame* pCurr) {
 	}
 	else {
 		std::chrono::high_resolution_clock::time_point tracking_start = std::chrono::high_resolution_clock::now();
-		std::cout << "tracker::start::" <<pCurr->mnFrameID<< std::endl;
-		//std::cout << mpMap->mpFirstKeyFrame->mpPlaneInformation->GetFloorPlane()->GetParam() << std::endl << std::endl << std::endl;
 		/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 		/////////Optical Flow Matching
 		////MatchInfo 설정
@@ -165,20 +163,15 @@ void UVR_SLAM::Tracker::Tracking(Frame* pPrev, Frame* pCurr) {
 		std::vector<int> vnIDXs, vnMPIDXs;
 		cv::Mat debugImg;
 		cv::Mat overlap = cv::Mat::zeros(pCurr->mnHeight, pCurr->mnWidth, CV_8UC1);
-		//mnPointMatching = mpMatcher->OpticalMatchingForTracking(mpRefKF, pCurr, vpTempCPs, vpTempMPs, vpTempPts, vbTempInliers, vnIDXs, overlap); //pCurr
-		mnPointMatching = mpMatcher->OpticalMatchingForTracking(pPrev, pCurr, vpTempCPs, vpTempPts); //pCurr
-		std::cout << "Tracking::" << pCurr->mnFrameID << " Match=" << mnPointMatching << std::endl;
+		mnPointMatching = mpMatcher->OpticalMatchingForTracking(mpRefKF, pCurr, vpTempCPs, vpTempMPs, vpTempPts, vbTempInliers, vnIDXs, overlap); //pCurr
 		//mpSystem->mbTrackingEnd = true;
 		std::chrono::high_resolution_clock::time_point tracking_a = std::chrono::high_resolution_clock::now();
 
 		//graph-based
 		{
 			std::unique_lock<std::mutex> lock(mpMap->mMutexMapUdpate);
-			//mnMapPointMatching = Optimization::PoseOptimization(pCurr, vpTempMPs, vpTempPts, vbTempInliers, vnMPIDXs);
-			mnMapPointMatching = Optimization::PoseOptimization(pCurr, vpTempCPs, vpTempPts);
-			std::cout << "Tracking::" << pCurr->mnFrameID << " PoseOpt=" << mnMapPointMatching << std::endl;
-			//int nMP = UpdateMatchingInfo(mpRefKF, pCurr, vpTempCPs,vpTempMPs, vpTempPts, vbTempInliers, vnIDXs, vnMPIDXs);
-			int nMP = UpdateMatchingInfo(pCurr, vpTempCPs, vpTempPts);
+			mnMapPointMatching = Optimization::PoseOptimization(pCurr, vpTempMPs, vpTempPts, vbTempInliers, vnMPIDXs);
+			int nMP = UpdateMatchingInfo(mpRefKF, pCurr, vpTempCPs, vpTempMPs, vpTempPts, vbTempInliers, vnIDXs, vnMPIDXs);
 		}
 		///////////////////////////////////////////////////////////////////////////////
 		/////////////////////////////////////////////키프레임 체크
